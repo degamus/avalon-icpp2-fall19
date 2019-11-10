4eea362aa2b2edefaedd21bf9b4e400584b5fe5e
degamus <RedCrafting.SIM@gmail.com>
2019-11-10 12:01:16 +0300

    Update task.cpp

--- task.cpp
+++ task.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "random.hpp"
+#include "extention.hpp"
 #include <string>
 using namespace ext;
 using namespace std;
@@ -12,13 +12,18 @@ struct Student
 	string Name;
 };
 
+struct Agent
+{
+	int monthlySales[12];
+};
+
 void Generator(Student data[], int size)
 {
 	for (int i = 0; i < size; i++)
 	{
 		data[i].Id = GetRandomValue(1, 1000);
 	}
-		
+
 	for (int i = 0; i < size; i++)
 	{
 		data[i].Graid = GetRandomValue(0, 100);;
@@ -111,15 +116,89 @@ void BetterTheBest(Student data[], int size)
 		SortedDataPack[i].Graid = data[i].Graid;
 	}
 
-	cout << "To be better than 25% you need to get " << SortedDataPack[(N / 4) + 1].Graid << endl;
-	cout << "To be better than 50% you need to get " << SortedDataPack[(N / 2) + 1].Graid << endl;
-	cout << "To be better than 75% you need to get " << SortedDataPack[(N - (N / 4)) + 1].Graid << endl;
+	cout << "To be better than 25% you need to get at least" << SortedDataPack[(N / 4) + 1].Graid << endl;
+	cout << "To be better than 50% you need to get at least" << SortedDataPack[(N / 2) + 1].Graid << endl;
+	cout << "To be better than 75% you need to get at least" << SortedDataPack[(N - (N / 4)) + 1].Graid << endl;
 
 }
+
 int main()
 {
 	int SortSetting = 0;
+	int max = 0;
+	int LostMonths;
+	int counter = 0;
 	Student datapack[N];
+	Agent ListOfAgents[10];
+	int Median[10];
+
+	for (int i = 0; i < 10; i++)
+	{
+		LostMonths = GetRandomValue(1, 12);
+		for (int j = 0; j < 12; j++)
+		{
+			if (j >= LostMonths)
+			{
+				ListOfAgents[i].monthlySales[j] = -1;
+			}
+			else
+			{
+				ListOfAgents[i].monthlySales[j] = GetRandomValue(100, 1000);
+			}
+		}
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		for (int k = 1; k < 12; k++)
+		{
+			for (int j = 0; j < 11; j++)
+			{
+
+				if (ListOfAgents[i].monthlySales[j] < ListOfAgents[i].monthlySales[j+1])
+				{
+					int temp = ListOfAgents[i].monthlySales[j];
+					ListOfAgents[i].monthlySales[j] = ListOfAgents[i].monthlySales[j + 1];
+					ListOfAgents[i].monthlySales[j + 1] = temp;
+
+				}
+			}
+		}
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+
+		for (int j = 0; j < 12; j++)
+		{
+			if (ListOfAgents[i].monthlySales[j] != -1)
+			{
+				counter++;
+			}
+		}
+		
+		if (counter % 2 != 0)
+		{
+			Median[i] = ListOfAgents[i].monthlySales[counter / 2];
+		}
+		else
+		{
+			Median[i] = (ListOfAgents[i].monthlySales[counter / 2] + ListOfAgents[i].monthlySales[(counter / 2) - 1]) /2;
+		}
+		counter = 0;
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+
+		if (Median[i] > max)
+		{
+			max = Median[i];
+		}
+	}
+
+	cout << max;
+
 
 	Generator(datapack, N);
 
@@ -134,9 +213,9 @@ int main()
 	else
 		if (SortSetting == 2)
 		{
-		Sort(datapack, N, 2);
+			Sort(datapack, N, 2);
 		}
-		else 
+		else
 			if (SortSetting == 3)
 			{
 				Sort(datapack, N, 3);
